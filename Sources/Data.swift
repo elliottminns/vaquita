da963df00a1c3ddfc60728f80c8f57c550b54fc1
Elliott Minns <elliott.minns@me.com>
2016-02-21 13:09:54 +0000

    Made bytes public

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -22,12 +22,12 @@ enum EncodingError: ErrorType {
 
 public struct Data {
 
-    let bytes: [UInt8]
+    public let bytes: [UInt8]
 
     public var size: Int {
         return self.bytes.count * sizeof(UInt8)
     }
-    
+
     public init(bytes: [UInt8]) {
         self.bytes = bytes
     }
@@ -38,13 +38,13 @@ public struct Data {
 
     public func toString() throws -> String {
         var bytes = self.bytes
-        guard let str = String(bytesNoCopy: &bytes, 
-            length: bytes.count * sizeof(UInt8), 
+        guard let str = String(bytesNoCopy: &bytes,
+            length: bytes.count * sizeof(UInt8),
             encoding: UInt8.encodingType.encoding,
             freeWhenDone: false) else {
                 throw EncodingError.Failed
         }
-        
+
         return str
     }
 }
---
+++ Xcode/Vaquita/Vaquita/Vaquita.h
@@ -0,0 +1,19 @@
+//
+//  Vaquita.h
+//  Vaquita
+//
+//  Created by Elliott Minns on 19/02/2016.
+//  Copyright © 2016 Elliott Minns. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Vaquita.
+FOUNDATION_EXPORT double VaquitaVersionNumber;
+
+//! Project version string for Vaquita.
+FOUNDATION_EXPORT const unsigned char VaquitaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vaquita/PublicHeader.h>
+
+
